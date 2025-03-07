f91227c3c117f2e2a82fb443c79ca27939b82a68
Karan Randhawa <life@karan-randhawa.com>
2025-03-06 21:46:30 -0500

    Add project files.

---
+++ Gitscuit API/Controllers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gitscuit_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Gitscuit API is running.");
+        }
+    }
+}
